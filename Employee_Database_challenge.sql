6b855a8d38189b5c2ff5e4f13d65c9019bfd65e4
Kossakova <zkossakova@gmail.com>
2021-10-03 23:47:52 -0500

    Adding final

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -12,6 +12,10 @@ WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
 ORDER BY emp_no
 
 select * from retirement_titles
+limit 10
+
+select * from departments
+limit 10
 
 -- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (emp_no) emp_no,
@@ -23,7 +27,10 @@ FROM retirement_titles
 ORDER BY emp_no, to_date DESC;
 
 select * from unique_titles
+limit 10
 
+select count(*)
+from unique_titles
 
 SELECT COUNT(title), title
 INTO retiring_titles
@@ -32,7 +39,7 @@ GROUP BY title
 ORDER BY COUNT(title) DESC;
 
 select * from retiring_titles
-
+limit 10
 
 SELECT DISTINCT ON (emp_no)
 e.emp_no, 
@@ -52,3 +59,32 @@ WHERE (de.to_date = '9999-01-01')
 AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY emp_no
 
+select * from mentorship_eligibilty
+limit 10
+
+select count(*)
+from mentorship_eligibilty
+
+SELECT COUNT(title), title
+INTO mentorship_eligibilty_titles
+FROM mentorship_eligibilty
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+select *
+from mentorship_eligibilty_titles
+
+SELECT DISTINCT ON (emp_no)
+       ut.emp_no, 
+       ut.first_name, 
+	   ut.last_name,
+       ut.title, 
+	   s.salary
+INTO retirement_salary
+FROM unique_titles as ut
+LEFT JOIN salaries as s 
+ON ut.emp_no = s.emp_no
+ORDER BY emp_no;
+
+select * from retirement_salary
+limit 10
